17fbc53465fa0960bc553a77c62553911d6a9674
Peter Sujan <peterasujan@gmail.com>
2016-01-28 01:34:05 -0800

    Got followers/following working. Still need to add some error checking.

--- followers.py
+++ followers.py
@@ -10,19 +10,33 @@ auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
                            CONSUMER_KEY, CONSUMER_SECRET)
 api = twitter.Twitter(auth=auth)
 
-def get_followers(user, is_id = False):
-    """ Returns the ids of users who follow user
-    """
+def get_users(api_call, user, is_id = False, max_num = None):
     if is_id:
         params = {'user_id': user}
     else:
         params = {'screen_name': user}
-    followers = []
-    followers += api.followers.ids(**params)['ids']
-    return followers
+    params['count'] = 5000 # highest number of responses available
+    current = api_call(**params)
+    results = current['ids']
+    next_cursor = current['next_cursor']
+    while next_cursor != 0 and (not max_num or len(results) < max_num):
+        current = api_call(**params)
+        results += current['ids']
+        next_cursor = current['next_cursor']
+        
+    return results[0:max_num] if max_num else results
 
-def get_following(screen_name, user_id):
+
+def get_followers(user, is_id = False, max_num = None):
+    """ Returns the ids of users who follow user
+    """
+    return get_users(api.followers.ids, user = user,
+                     is_id = is_id, max_num = max_num)
+
+
+def get_following(user, is_id = False, max_num = None):
     """ Returns the ids of users followed by USER
     """
-    return None
+    return get_users(api.friends.ids, user = user,
+                     is_id = is_id, max_num = max_num)
 
