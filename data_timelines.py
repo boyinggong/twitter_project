abbe6672e181fcabb0ed6abf78214511892a52c4
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2016-01-30 23:00:59 -0800

    Upload new nominees spreadsheet

---
+++ data_timelines.py
@@ -0,0 +1,50 @@
+from __future__ import division, print_function
+
+import json
+from time import sleep
+import twitter
+
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+OAUTH_TOKEN = ''
+OAUTH_TOKEN_SECRET = ''
+
+auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
+                           CONSUMER_KEY, CONSUMER_SECRET)
+api = twitter.Twitter(auth=auth)
+
+def retrieve_timeline(screen_name):
+    """ Gets the twitter timeline of the user as specified by their twitter
+        screen_name e.g for the actor Leonardo DiCaprio use "LeoDiCaprio"
+        (without quotes) to fetch his timeline tweets
+    """
+    print("Beginning retrieval of " + screen_name)
+    try:
+        timeline = api.statuses.user_timeline(screen_name=screen_name,
+                                              count=200, include_rts=1)
+    except:
+        print("Reached rate limit; sleeping 15 minutes")
+        sleep(900)
+        timeline = api.statuses.user_timeline(screen_name=screen_name,
+                                              count=200, include_rts=1)
+
+    ntweets = len(timeline)
+    if ntweets < 200:
+        return timeline
+    while ntweets == 200:
+        min_id = min([tweet["id"] for tweet in timeline])
+        try:
+            next_timeline = api.statuses.user_timeline(screen_name=screen_name,
+                                                       count=200,
+                                                       max_id=min_id - 1,
+                                                       include_rts=1)
+        except:
+            print("Reached rate limit; sleeping 15 minutes")
+            sleep(900)
+            next_timeline = api.statuses.user_timeline(screen_name=screen_name,
+                                                       count=200,
+                                                       max_id=min_id - 1,
+                                                       include_rts=1)
+        ntweets = len(next_timeline)
+        timeline += next_timeline
+    return timeline
