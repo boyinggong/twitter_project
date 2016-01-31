3375fb6258ee795f09e850cc38344a589b1ca497
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2016-01-31 14:06:23 -0800

    Update fetch followers with additional follower metadata

--- fetch_followers.py
+++ fetch_followers.py
@@ -14,6 +14,11 @@ if __name__ == '__main__':
                     current = current[1:]
             screen_names.append(current)
 
-    following = get_all_followers(screen_names, max_num = 5000)
-    with open('data/output_data/followers/followers.json', 'w') as f:
-        json.dump(following, f, indent = 4)
+    followers = get_all_followers(screen_names, max_num = 15000)
+
+    computed = "data/output_data/followers_computed_data/"
+    with open(computed + 'followers.json', 'w') as f:
+        json.dump(followers, f, indent = 4)
+
+    with open(computed + "screen_names.json") as f:
+        json.dump(screen_names, f, indent = 4)
