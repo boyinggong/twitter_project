381f2b36b60d2a699c53858ffd3f478d6a791db4
Peter Sujan <peterasujan@gmail.com>
2016-02-04 20:47:12 -0800

    Follower vs. following looking quite good.

--- rscripts/follow_plots.R
+++ rscripts/follow_plots.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(ggplot2)
 
 ## Pull in the data
-source("../read_data.R")
+source("read_data.R")
 
 combined$tweet_user_followers_count <- as.numeric(combined$tweet_user_followers_count)
 combined$tweet_user_friends_count <- as.numeric(combined$tweet_user_friends_count)
@@ -16,22 +16,24 @@ follow <- select(combined,
                  which(names(combined) %in% follow_columns))
 #follow <- select(combined, one_of(follow_columns))
 follow <- distinct(follow)
+follow.interesting <- filter(follow,
+                             tweet_user_friends_count < 40000 &
+                               (tweet_user_followers_count > 25e5 |
+                                  tweet_user_friends_count > 1000))
 
 p <- ggplot(data = filter(follow, tweet_user_friends_count < 40000),
             aes(x = tweet_user_followers_count,
                 y = tweet_user_friends_count,
                 color = WINNER_FLAG))
 p <- p + geom_point()
-follow.interesting <- filter(follow,
-                             tweet_user_friends_count < 40000 &
-                             (tweet_user_followers_count > 25e5 |
-                             tweet_user_friends_count > 1000))
 p <- p + geom_label(data = follow.interesting,
                     aes(label = NOMINEE, hjust = "left"),
                     #nudge_x = 1020000,
-                    nudge_x = 10000,
+                    nudge_x = 200000,
                     show.legend = FALSE)
-p <- p + xlab("Number of followers") + ylab("Number followed")
+p <- p + labs(x = "Number of followers", y = "Number followed",
+              title = "Nominee Following vs. Follower Counts")
+  
+p <- p + coord_cartesian(xlim = c(0, 1.75e7)) + scale_colour_discrete(name = "Result")
 p
 
-follow[order(follow$tweet_user_friends_count), -4]
