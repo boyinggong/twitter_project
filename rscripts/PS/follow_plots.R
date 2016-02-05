092533f4c1c86d2a5418d4c5b65cc9532bce0998
Peter Sujan <peterasujan@gmail.com>
2016-02-04 20:12:31 -0800

    Working on plotting.

--- rscripts/PS/follow_plots.R
+++ rscripts/PS/follow_plots.R
@@ -4,15 +4,34 @@ library(ggplot2)
 ## Pull in the data
 source("../read_data.R")
 
+combined$tweet_user_followers_count <- as.numeric(combined$tweet_user_followers_count)
+combined$tweet_user_friends_count <- as.numeric(combined$tweet_user_friends_count)
 follow_columns <- c("tweet_user_screen_name",
-                    #"WINNER_FLAG",
-                    #"CATEGORY",
-                    #"tweet_user_followers_count",
+                    "WINNER_FLAG",
+                    "CATEGORY",
+                    "NOMINEE",
+                    "tweet_user_followers_count",
                     "tweet_user_friends_count")
 follow <- select(combined,
                  which(names(combined) %in% follow_columns))
 #follow <- select(combined, one_of(follow_columns))
 follow <- distinct(follow)
-p <- ggplot(data = follow, aes(x = tweet_user_followers_count,
-                                 y = tweet_user_friends_count)) 
-p + geom_point()
+
+p <- ggplot(data = filter(follow, tweet_user_friends_count < 40000),
+            aes(x = tweet_user_followers_count,
+                y = tweet_user_friends_count,
+                color = WINNER_FLAG))
+p <- p + geom_point()
+follow.interesting <- filter(follow,
+                             tweet_user_friends_count < 40000 &
+                             (tweet_user_followers_count > 25e5 |
+                             tweet_user_friends_count > 1000))
+p <- p + geom_label(data = follow.interesting,
+                    aes(label = NOMINEE, hjust = "left"),
+                    #nudge_x = 1020000,
+                    nudge_x = 10000,
+                    show.legend = FALSE)
+p <- p + xlab("Number of followers") + ylab("Number followed")
+p
+
+follow[order(follow$tweet_user_friends_count), -4]
--- rscripts/read_data.R
+++ rscripts/read_data.R
@@ -1,8 +1,10 @@
 library(readr)
 library(dplyr)
 
+
+## for some reason, read_csv doesn't work here
 timelines <- read.csv("../../data/output_data/timelines/CSV/all_concat_timelines_20160203.csv",
                       header = TRUE, stringsAsFactors = FALSE)
-metadata <- read_csv("../../data/input_data/golden_globes_metadata/nominees-spreadsheet-01-30.csv")
+metadata <- read_csv("../../data/input_data/golden_globes_metadata/nominees-spreadsheet-02-04.csv")
 
 combined <- left_join(timelines, metadata, by = c("tweet_user_screen_name" = "TWITTER_SCREEN_NAME"))
