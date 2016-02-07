b2581e6bc58cf9c4faf4f8b13c9e8f4184001961
Peter Sujan <peterasujan@gmail.com>
2016-02-06 16:59:33 -0800

    Followers vs. following now super pretty.

--- rscripts/follow_plots.R
+++ rscripts/follow_plots.R
@@ -16,7 +16,7 @@ follow <- select(combined,
                  which(names(combined) %in% follow_columns))
 #follow <- select(combined, one_of(follow_columns))
 follow <- distinct(follow)
-follow.interesting <- filter(follow,
+follow_interesting <- filter(follow,
                              tweet_user_friends_count < 40000 &
                                (tweet_user_followers_count > 25e5 |
                                   tweet_user_friends_count > 1000))
@@ -25,15 +25,29 @@ p <- ggplot(data = filter(follow, tweet_user_friends_count < 40000),
             aes(x = tweet_user_followers_count,
                 y = tweet_user_friends_count,
                 color = WINNER_FLAG))
+p <- p + geom_hline(yintercept = 1000) + geom_vline(xintercept = 25e5)
 p <- p + geom_point()
-p <- p + geom_label(data = follow.interesting,
+p <- p + geom_label(data = follow_interesting,
                     aes(label = NOMINEE, hjust = "left"),
-                    #nudge_x = 1020000,
                     nudge_x = 200000,
                     show.legend = FALSE)
 p <- p + labs(x = "Number of followers", y = "Number followed",
               title = "Nominee Following vs. Follower Counts")
   
 p <- p + coord_cartesian(xlim = c(0, 1.75e7)) + scale_colour_discrete(name = "Result")
+p <- p + annotate("text", x = 0, y = 2000, label = "Try-hards",
+                  size = 7, hjust = "left")
+p <- p + annotate("text", x = 01e7, y = 500, label = "Popular kids",
+                  size = 7, hjust = "left")
 p
 
+
+## not sure if this plot is very interesting
+p <- ggplot(data = filter(follow, tweet_user_friends_count < 40000 &
+                            grepl("Best Actress", CATEGORY)),
+            aes(x = tweet_user_followers_count,
+                y = tweet_user_friends_count,
+                color = WINNER_FLAG))
+p <- p + geom_point()
+#p <- p + facet_wrap(~CATEGORY) + scale_y_log10()
+p
