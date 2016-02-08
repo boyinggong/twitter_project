c8699f69096c6157f25eca167fa6b095a5cb118e
Tomofumi Ogawa <togawa28@berkeley.edu>
2016-02-07 22:22:45 -0800

    corrected mistake in pre_post_function

--- rscripts/pre_post_function.R
+++ rscripts/pre_post_function.R
@@ -49,8 +49,8 @@ pre_post_modify <- function(data){
   data.modified <- data.modified %>%
     rowwise() %>% 
     mutate(pre_post_during_flag =
-             ifelse(tweet_time_lag_days > -1,
-                    ifelse(tweet_time_lag_days > 1, "during", "pre"), "post")
+             ifelse(tweet_time_lag_days < -1, "pre",
+                    ifelse(tweet_time_lag_days <= 1, "during", "post"))
     )
   
   return(data.modified)
