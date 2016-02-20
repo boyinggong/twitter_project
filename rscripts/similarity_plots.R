4ce4ea20648ecde2337549442ad55e2b338ccfea
Peter Sujan <peterasujan@gmail.com>
2016-02-19 23:01:08 -0800

    Added images and cleaned up similarity plot. Added latex of poster explanation.

--- rscripts/similarity_plots.R
+++ rscripts/similarity_plots.R
@@ -23,6 +23,7 @@ joined <- data.frame(tweet_user_screen_name = names_vector,
                      stringsAsFactors = FALSE) %>%
                      left_join(combined[c("tweet_user_screen_name", "NOMINEE")],
                                by = "tweet_user_screen_name") %>% distinct()
+joined$NOMINEE[joined$tweet_user_screen_name == "spythemovieuk"] <- "Spy (UK account)"
 rownames(dot_products) <- colnames(dot_products) <- joined$NOMINEE
 
 ## ---- similarityHist ----
@@ -51,11 +52,34 @@ pair_names <- sapply(top_pairs, extract_pair_names)
 
 pairs_df <- data.frame(Pair = pair_names, Similarity = top_pairs)
 pairs_df <- pairs_df %>% mutate(Pair = factor(Pair, levels = Pair))
+notable_pairs <- rep(1, nrow(pairs_df))
+notable_pairs[c(2, 6, 8, 15)] <- 2
+pairs_df <- pairs_df %>% mutate(Notable = factor(notable_pairs))
+
+imgAnnotate <- function(filename, ...) {
+  img  <- readJPEG(filename)
+  g <- rasterGrob(img, interpolate = TRUE)
+  annotation_custom(g, ...)
+}
+
+
+
 p <- ggplot(data = pairs_df,
-            aes(x = Pair, y = Similarity))
+            aes(x = Pair, y = Similarity, fill = Notable))
 p <- p + geom_bar(stat = "identity") + coord_flip()
+p <- p + ylim(0, 0.6)
+## Note: coordinates have been flipped, so x and y adjustments are also flipped
+p <- p + imgAnnotate("../poster_graphics/pictures/thebigshortcsheader.jpg",
+                     xmin = 13, xmax = 16, ymin = 0.41, ymax = 0.51)
+p <- p + imgAnnotate("../poster_graphics/pictures/pixar_movies.jpg",
+                     xmin = 4, xmax = 7, ymin = 0.21, ymax = 0.31)
+p <- p + imgAnnotate("../poster_graphics/pictures/american_crime.jpg",
+                     xmin = 7, xmax = 10, ymin = 0.25, ymax = 0.35)
+p <- p + imgAnnotate("../poster_graphics/pictures/elba_henson.jpg",
+                     xmin = 1, xmax = 4, ymin = 0.2, ymax = 0.3)
 p <- p + ggtitle("Following Similarity: Most Similar Pairs")
+p <- p + theme(legend.position = "none")
 p
 #dev.off()
 ggsave(filename = "../poster_graphics/similarity.svg",
-       plot = p, width = 15, height = 30, units = "cm")
+       plot = p, width = 50, height = 30, units = "cm")
