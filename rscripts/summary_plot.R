adbfb59aadc05cfb3b8f23c119d261ef0070e789
Peter Sujan <peterasujan@gmail.com>
2016-02-23 14:09:49 -0800

    Updates to summary plot.

--- rscripts/summary_plot.R
+++ rscripts/summary_plot.R
@@ -36,14 +36,16 @@ p <- p + geom_bar(stat = "identity", aes(fill = GENDER_FLAG))
 p <- p + ylab("Number of Tweets")
 p <- p + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 p <- p + facet_wrap(~ FILM_FLAG, scales = "free_x")
-p <- p + ggtitle("Data Overview") + xlab("Nominees")
+p <- p + ggtitle("Clean Data Overview") + xlab("Nominees")
 p <- p + ylim(c(0, 500))
 p <- p + scale_fill_discrete(name = "User Type")
 p <- p + theme(axis.text.y = element_text(size=15, color="black")) +
   theme(axis.text.x = element_text(size=15, color="black")) +
   theme(plot.title = element_text(lineheight=3, face="bold",color="black", size=29)) +
   theme(axis.title.y = element_text(size = rel(1.8))) +
-  theme(axis.title.x = element_text(size = rel(1.8)))
+  theme(axis.title.x = element_text(size = rel(1.8))) +
+  theme(strip.text.x = element_text(size=15, color="black"))
+
 p
 #dev.off()
 ggsave(filename = "../poster_graphics/summary_plot.png",
